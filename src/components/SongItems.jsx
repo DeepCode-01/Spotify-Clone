f29dc566cc2d7b6be812c70d1b37b8bc18113956
Deepali Verma <deepaliverma064@gmail.com>
2024-09-26 10:50:32 +0530

    adding song click functionality

--- src/components/SongItems.jsx
+++ src/components/SongItems.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import  { PlayerContext } from '../context/PlayerContext'
 
 const SongItems = ({name,desc,image,id}) => {
+
+  const {playWithId} = useContext(PlayerContext);
+  
   return (
     <div>
-      <div className="min-w-[150px] p-2 px3 rounded cursor-pointer hover:bg-[#ffffff26]">
+      <div onClick={()=>playWithId(id)} className="min-w-[150px] p-2 px3 rounded cursor-pointer hover:bg-[#ffffff26]">
          <img className='rounded' src={image} alt="" />
          <p className='font-bold mt-2 mb1'>{name}</p>
          <p className='text-slate-200 text-sm'>{desc}</p>
--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -32,6 +32,43 @@ const PlayerContextProvider = (props)=>{
         setPlayStatus(false)
     }
 
+const playWithId = async (id) =>{
+  await setTrack(songsData[id]);
+  await audioRef.current.play();
+  setPlayStatus(true)
+}
+
+
+    useEffect(() => {
+
+        if (audioRef.current) {
+            audioRef.current.addEventListener("loadedmetadata", () => {
+            
+                setTimeout(() => {
+                    audioRef.current.ontimeupdate = () => {
+                        // Ensure both currentTime and duration are valid before accessing them
+                        if (audioRef.current && audioRef.current.currentTime && audioRef.current.duration) {
+                            seekBar.current.style.width = Math.floor(
+                                (audioRef.current.currentTime / audioRef.current.duration) * 100
+                            ) + "%";
+                            
+                            setTime({
+                                currentTime: {
+                                    second: Math.floor(audioRef.current.currentTime % 60),
+                                    minute: Math.floor(audioRef.current.currentTime / 60),
+                                },
+                                totalTime: {
+                                    second: Math.floor(audioRef.current.duration % 60),
+                                    minute: Math.floor(audioRef.current.duration / 60),
+                                },
+                            });
+                        }
+                    };
+                }, 1000);
+            });
+        }
+    }, [audioRef]);
+
     const contextValue = {
 
       audioRef,
@@ -44,7 +81,8 @@ const PlayerContextProvider = (props)=>{
       time,
       setTime,
       play,
-      pause
+      pause,
+      playWithId
 
     }
 
